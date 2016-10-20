48bcb36df55ca7077396dc018d9d8a93c2fad2cc
Christopher Wan <christopher.wan@yale.edu>
2016-10-20 15:00:54 -0400

    PLS

--- splash/views.py
+++ splash/views.py
@@ -53,33 +53,34 @@ def authenticate():
         print "success"
         return request.args.get('hub.challenge')
         return render_template('success.html')
-    print "Handling Messages"
-    payload = request.get_data()
-    print payload
-    for sender, message in messaging_events(payload):
-        print "Incoming from %s: %s" % (sender, message)
-        send_message(PAT, sender, message)
-        return "ok"
+    try:
+        data = json.loads(request.data)
+        text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
+        sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
+        payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
+        r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=payload) # Lets send it
+    except Exception as e:
+        print traceback.format_exc() # something went wrong
 
-def messaging_events(payload):
-    data = json.loads(payload)
-    messaging_events = data["entry"][0]["messaging"]
-    for event in messaging_events:
-        if "message" in event and "text" in event["message"]:
-            yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-        else:
-            yield event["sender"]["id"], "I can't echo this"
+# def messaging_events(payload):
+#     data = json.loads(payload)
+#     messaging_events = data["entry"][0]["messaging"]
+#     for event in messaging_events:
+#         if "message" in event and "text" in event["message"]:
+#             yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+#         else:
+#             yield event["sender"]["id"], "I can't echo this"
 
-def send_message(token, recipient, text):
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-        params={"access_token": token},
-        data=json.dumps({
-        "recipient": {"id": recipient},
-        "message": {"text": text.decode('unicode_escape')}
-        }),
-        headers={'Content-type': 'application/json'})
-    if r.status_code != requests.codes.ok:
-        print r.text
+# def send_message(token, recipient, text):
+#     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+#         params={"access_token": token},
+#         data=json.dumps({
+#         "recipient": {"id": recipient},
+#         "message": {"text": text.decode('unicode_escape')}
+#         }),
+#         headers={'Content-type': 'application/json'})
+#     if r.status_code != requests.codes.ok:
+#         print r.text
 
 
 @splash.route('/error', methods=['GET'])
