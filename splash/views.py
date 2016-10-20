3533ae527d516329e9190ec44f94a7bd3553ed02
Christopher Wan <christopher.wan@yale.edu>
2016-10-20 14:54:06 -0400

    pls

--- splash/views.py
+++ splash/views.py
@@ -16,7 +16,7 @@ import os
 from datetime import datetime
 
 splash = Blueprint('splash', __name__, template_folder="templates")
-
+access_token = os.environ['CS490_MESSENGER_TOKEN']
 
 @app.before_request
 def before_request():
@@ -52,6 +52,34 @@ def authenticate():
         print "success"
         return request.args.get('hub.challenge')
         return render_template('success.html')
+    print "Handling Messages"
+    payload = request.get_data()
+    print payload
+    for sender, message in messaging_events(payload):
+        print "Incoming from %s: %s" % (sender, message)
+        send_message(PAT, sender, message)
+        return "ok"
+
+def messaging_events(payload):
+    data = json.loads(payload)
+    messaging_events = data["entry"][0]["messaging"]
+    for event in messaging_events:
+        if "message" in event and "text" in event["message"]:
+            yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+        else:
+            yield event["sender"]["id"], "I can't echo this"
+
+def send_message(token, recipient, text):
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+        params={"access_token": token},
+        data=json.dumps({
+        "recipient": {"id": recipient},
+        "message": {"text": text.decode('unicode_escape')}
+        }),
+        headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
 
 @splash.route('/error', methods=['GET'])
 def error():
