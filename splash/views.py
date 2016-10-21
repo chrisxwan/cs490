568687a7008e9fc3ccd6bef618bba0d19ac28a29
Christopher Wan <christopher.wan@yale.edu>
2016-10-21 17:09:53 -0400

    intermediate page

--- splash/views.py
+++ splash/views.py
@@ -74,20 +74,26 @@ def authenticate():
                 payload = {'recipient': {'id': sender}, 'message': {'text': "Your account has been verified!"}} # We're going to send this back
                 r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=payload) # Lets send it
                 user = User.query.filter(User.facebook_code == text).first()
+                user.facebook_confirmation_status = 1
                 user.facebook_id = sender
                 print user.facebook_id
                 db.session.commit()
                 return 'committed'
         else:
             user = User.query.filter(User.facebook_id == sender).first()
-            if ((datetime.now() - user.last_login_attempt).seconds > 86400):
+            if ((datetime.now() - user.last_login_attempt).seconds < 300):
                 payload = {'recipient': {'id': sender}, 'message': {'text': "Success!"}} # We're going to send this back
                 r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=payload) # Lets send it
                 user.last_login_attempt = None
+                user.last_successful_login = datetime.now()
                 db.session.commit()
                 print "success"
                 login_user(user)
                 return 'success'
+            else:
+                payload = {'recipient': {'id': sender}, 'message': {'text': "Failed to authenticate in time! Login again."}} # We're going to send this back
+                r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + access_token, json=payload) # Lets send it
+                return "failed to login in time"
 
     except Exception as e:
         print traceback.format_exc() # something went wrong
@@ -185,6 +191,9 @@ def create():
 
     return redirect(url_for('splash.submitted', code="1", email=a.email))
 
+@splash.route('/authenticate_facebook', methods=['GET'])
+def authenticate_facebook():
+    return render_template('authenticate_facebook.html')
 
 @splash.route('/login', methods=['POST'])
 def login():
@@ -206,8 +215,8 @@ def login():
         return redirect(url_for('splash.confirm_facebook', token=user.facebook_code))
     if ((datetime.now() - user.last_successful_login).seconds > 3600):
         user.last_login_attempt = datetime.now()
-        redirect(url_for('splash.index'))
-    redirect(url_for('splash.success'))
+        return redirect(url_for('splash.authenticate_facebook'))
+    return redirect(url_for('splash.success'))
 
     # login_user(user)
     
