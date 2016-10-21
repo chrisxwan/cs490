7b48841eb3fb13be63f6c4cc4c1ff9feeea99f0a
Christopher Wan <christopher.wan@yale.edu>
2016-10-21 16:32:34 -0400

    fix reset

--- reset.py
+++ reset.py
@@ -5,7 +5,7 @@ from user.models import *
 db.drop_all()
 db.create_all()
 
-u = User(email="chrisxwan@gmail.com", firstname="Christopher", lastname="Wan", email_confirmation_status=1, facebook_confirmation_status = 0, facebook_code=123)
+u = User(email="chrisxwan@gmail.com", firstname="Christopher", lastname="Wan", email_confirmation_status=1, facebook_confirmation_status = 0, facebook_code="123")
 u.hash_password('helloworld')
 db.session.add(u)
 db.session.commit()
