a017c09f3827990595f376233b48ecb65ceaa90b
Christopher Wan <christopher.wan@yale.edu>
2016-10-31 21:22:25 -0400

    testing polling
    fix
    
    another fix
    
    add safe
    
    another shot
    
    pls
    
    again
    
    add script
    
    edit jquery
    
    last resort

---
+++ static/javascript/poll.js
@@ -0,0 +1,18 @@
+	
+$(document).ready(function() {
+	function doPoll() {
+		console.log('http://cs490-project.herokuapp.com/check_db?email=' + "{{ email }}");
+		var post_html = 'http://cs490-project.herokuapp.com/check_db?email=' + $("#my-data").attr('data-name');
+		console.log(post_html);
+		$.post(post_html, function(data) {
+		    console.log(data);
+		    if (data === "1") {
+		      location.href = 'http://cs490-project.herokuapp.com/success';
+		    }
+		    setTimeout(doPoll,500);
+		});
+	}
+
+	console.log("here");
+	doPoll();
+})
